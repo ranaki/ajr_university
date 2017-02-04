fea39d95a4fac9db5de9a685a0bf4adf0ab32f4e
rick baker <baker.rickw@gmail.com>
2017-02-03 21:49:44 -0700

    first commit of school

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var fs = require('fs');
+var ejs = require('ejs');
+var app = express();
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
+app.get('/courses', function(request, response) {
+    fs.readFile('courses.json', 'utf8', function(err, data) {
+    var courses = JSON.parse(data);
+    response.locals = {courses: courses};
+    response.render('courses.ejs');
+    });
+});
+
+app.get('/courses/:id', function(request, response) {
+    fs.readFile('courses.json', 'utf8', function(err, data) {
+    var coursesParsed = JSON.parse(data);
+    var course = coursesParsed.filter( function(obj) {
+        return obj.id === parseInt(request.params.id);
+    })[0];
+
+    response.locals = {course: course};
+    response.render('course.ejs');
+    });
+});
+
+app.get('/contacts', function(request, response) {
+    response.sendFile(__dirname + '/contacts.html');
+});
+app.listen(8000);
+
